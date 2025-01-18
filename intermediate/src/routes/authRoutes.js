3c5603a3d25f20fe2e171209780af1d7b32b0f46
Richard Abishai <rickoshade1891@gmail.com>
2025-01-18 20:34:17 +0530

    Add authentication and todo routes with initial setup

---
+++ intermediate/src/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import db from '../db.js';
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+
+})
+
+router.post("/login", (req, res) => {
+
+})
+
+
+export default router;
---
+++ intermediate/src/routes/todoRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import db from "db.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => { })
+
+router.post("/", (req, res) => { })
+
+router.put("/:id", (req, res) => { })
+
+router.delete("/:id", (req, res) => { })
+
+export default router;
