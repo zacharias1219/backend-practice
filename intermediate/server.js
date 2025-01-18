bd7427fc9926995dbc22dcec1900762ba498a7d5
Richard Abishai <rickoshade1891@gmail.com>
2025-01-18 20:15:39 +0530

    Add static file serving and initial styles for the application

--- intermediate/server.js
+++ intermediate/server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 const PORT = process.env.PORT || 8000
 const app = express()
 
+app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+app.use(express.static(path.join(__dirname, "../public")))
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
 })
