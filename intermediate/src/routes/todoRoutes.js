2f9fef50f26a6e7070a3abbad6c1767eebc7a032
Richard Abishai <rickoshade1891@gmail.com>
2025-01-24 22:51:09 +0530

    Implement CRUD operations for todos: add, update, and delete routes

--- intermediate/src/routes/todoRoutes.js
+++ intermediate/src/routes/todoRoutes.js
@@ -9,10 +9,32 @@ router.get("/", (req, res) => {
     res.json(todos)
  })
 
-router.post("/", (req, res) => { })
+router.post('/', (req, res) => {
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
 
-router.put("/:id", (req, res) => { })
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+})
 
-router.delete("/:id", (req, res) => { })
+router.put('/:id', (req, res) => {
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
 
-export default router;
+    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+    updatedTodo.run(completed, id)
+
+    res.json({ message: "Todo completed" })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
+    
+    res.send({ message: "Todo deleted" })
+})
+
+export default router
