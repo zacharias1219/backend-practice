6d5b6e20f6953a5860a3fdd543a4d98568c4f5ec
Richard Abishai <rickoshade1891@gmail.com>
2025-01-24 23:18:18 +0530

    Set up Docker and Docker Compose for the application; refactor routes to use Prisma

--- hard/src/routes/authRoutes.js
+++ hard/src/routes/authRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import db from '../db.js';
 import prisma from '../prismaClient.js';
 
 const router = express.Router();
--- hard/src/routes/todoRoutes.js
+++ hard/src/routes/todoRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import db from "../db.js";
 import prisma from '../prismaClient.js';
 
 const router = express.Router();
