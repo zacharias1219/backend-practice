1361a136e1220769e5f9cf82b5bffa7b86da732c
Richard Abishai <rickoshade1891@gmail.com>
2025-01-24 23:07:59 +0530

    Refactor todo routes to use Prisma for database operations

--- hard/src/routes/todoRoutes.js
+++ hard/src/routes/todoRoutes.js
@@ -1,38 +1,59 @@
 import express from 'express';
 import db from "../db.js";
+import prisma from '../prismaClient.js';
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
-    const todos = getTodos.all(req.userId)
+router.get("/", async (req, res) => {
+    const todos = await prisma.todo.findMany({
+        where: {
+            userId: req.userId
+        }  
+    })
     res.json(todos)
- })
-
-router.post('/', (req, res) => {
+})
+router.post('/', async (req, res) => {
     const { task } = req.body
-    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
-    const result = insertTodo.run(req.userId, task)
+    const todo = await prisma.todo.create({
+        data: {
+            task,
+            user: {
+                connect: {
+                    id: req.userId
+                }
+            }
+        }
+    })
 
-    res.json({ id: result.lastInsertRowid, task, completed: 0 })
+    res.json(todo)
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
     const { completed } = req.body
     const { id } = req.params
     const { page } = req.query
 
-    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
-    updatedTodo.run(completed, id)
+    const updatedTodo = await prisma.todo.update({
+        where: { 
+            id: parseInt(id),
+            userId: req.userId
+        },
+        data: { completed: !!completed }
+    })
 
-    res.json({ message: "Todo completed" })
+    res.json(updatedTodo)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
     const { id } = req.params
     const userId = req.userId
-    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
-    deleteTodo.run(id, userId)
+    
+    await prisma.todo.deleteMany({
+        where: {
+            id: parseInt(id),
+            userId
+        }
+    })
     
     res.send({ message: "Todo deleted" })
 })
